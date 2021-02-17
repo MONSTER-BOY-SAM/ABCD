f123b070238d41f7ca3dfa05a470396eb13b59ac
MONSTER-BOY-SAM <73885359+MONSTER-BOY-SAM@users.noreply.github.com>
2021-02-17 12:07:56 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,42 @@
+class Player {
+  constructor(){
+
+    this.name = null;
+    this.distance = 0;
+    this.index = 0;
+
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance : this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+
+    var pir = database.ref('players');
+    pir.on("value",(data)=>{
+
+      allplayers = data.val();
+
+    })
+
+  }
+
+}
